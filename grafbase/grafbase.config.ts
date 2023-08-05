b55027ed545b3c4f70e570933d0fa691401107f2
Anish Kashyap <anishkashyap311@gmail.com>
2023-08-05 11:45:20 +0530

    initial commit

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,40 +1,24 @@
 import { g, auth, config } from "@grafbase/sdk";
 
-const post = g
-    .model("Post", {
-        title: g.string(),
-        slug: g.string().unique(),
-        content: g.string().optional(),
-        publishedAt: g.datetime().optional(),
-        comments: g
-            .relation(() => comment)
-            .optional()
-            .list()
-            .optional(),
-        likes: g.int().default(0),
-        tags: g.string().optional().list().length({ max: 5 }),
-        author: g.relation(() => user).optional(),
-    })
-    .search();
-
-const comment = g.model("Comment", {
-    post: g.relation(post),
-    body: g.string(),
-    likes: g.int().default(0),
-    author: g.relation(() => user).optional(),
+const User = g.model("User", {
+    name: g.string().length({ min: 3, max: 20 }),
+    email: g.email(),
+    avatarUrl: g.url(),
+    description: g.string().optional(),
+    gitHubUrl: g.url().optional(),
+    linikedInUrl: g.url(),
+    projects: g.relation(() => Project).list(),
 });
 
-const user = g.model("User", {
-    name: g.string(),
-    email: g.email().optional(),
-    posts: g.relation(post).optional().list(),
-    comments: g.relation(comment).optional().list(),
-
-    // Extend models with resolvers
-    // https://grafbase.com/docs/edge-gateway/resolvers
-    // gravatar: g.url().resolver('user/gravatar')
+const Project = g.model("Project", {
+    title: g.string().length({ min: 3 }),
+    description: g.string(),
+    image: g.url(),
+    liveSiteUrl: g.url(),
+    gitHubUrl: g.url(),
+    category: g.string().search(),
+    createdBy: g.relation(() => User),
 });
-
 export default config({
     schema: g,
     // Integrate Auth
